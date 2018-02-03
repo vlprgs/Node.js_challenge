4115b7f1fff514e01387c42d40277cf2b7872ca4
vlprgs <rafal.soloniewicz@gmail.com>
2018-02-03 15:23:31 +0100

    upd2
    upd2

--- app/zadanieDnia.js
+++ app/zadanieDnia.js
@@ -4,3 +4,4 @@
         array.forEach((element) => {
             setTimeout(() => {console.log(element)},parseInt(element,10)*1000);
         });
+        //toDo: przy większych liczbach zajmuje to wiecej czasu, więc lepiej zmienic timeout na stały, niski i niezalezny od wprowadzonej cyfry
